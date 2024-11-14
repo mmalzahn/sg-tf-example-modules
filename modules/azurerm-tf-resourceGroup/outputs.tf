5c2d8fdefedb74331bfd38e00168f761e351fa41
Matthias Malzahn <matthias.malzahn@stackguardian.io>
2024-11-14 20:24:29 +0700

    add more outputs to the module

--- modules/azurerm-tf-resourceGroup/outputs.tf
+++ modules/azurerm-tf-resourceGroup/outputs.tf
@@ -6,10 +6,20 @@ output "outputresource" {
   value = azurerm_resource_group.test_rg
 }
 
+output "resource_group_name" {
+  value = azurerm_resource_group.test_rg.name
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.test_rg.id
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.test_rg.location
+}
 
 
 ## Example:
-
 
 # output "outputname" {
 #     value = var.varname
