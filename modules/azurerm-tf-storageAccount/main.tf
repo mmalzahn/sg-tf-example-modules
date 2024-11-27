582a4bc15daff679a3027acdb34ca9ac0faf0617
Matthias Malzahn <matthias.malzahn@stackguardian.io>
2024-11-27 16:19:08 +0700

    add random provider for random part of SA name

--- modules/azurerm-tf-storageAccount/main.tf
+++ modules/azurerm-tf-storageAccount/main.tf
@@ -5,10 +5,16 @@
 *
 */
 
+resource "random_string" "sa_postfix" {
+  length = 3
+  lower = true
+}
+
 resource "azurerm_storage_account" "sa_main" {
   resource_group_name = var.rgname
+  https_traffic_only_enabled = var.https-traffic-only-enabled
   location = var.region
-  name = "sawefufiu3ii"
+  name = "sa${var.app-name}${random_string.sa_postfix.result}"
   account_tier = "Standard"
   account_replication_type = "GRS"
 }
